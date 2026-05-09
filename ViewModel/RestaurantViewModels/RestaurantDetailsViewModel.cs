129c87898336a91de787f49070f226517b91ca97
ziad Hany <zizo.juv7@gmail.com>
2026-05-09 19:39:10 +0300

    Crud for Restaurant (not tested)

---
+++ ViewModel/RestaurantViewModels/RestaurantDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using TalabatSmartVillage.ViewModel.MenuItemViewModels;
+
+namespace TalabatSmartVillage.ViewModel.RestaurantViewModels
+{
+    public class RestaurantDetailsViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CategoryName { get; set; }
+        public string Address { get; set; }
+        public bool IsOpen { get; set; }
+        public IEnumerable<MenuItemListViewModel> MenuItems { get; set; }
+    }
+}
